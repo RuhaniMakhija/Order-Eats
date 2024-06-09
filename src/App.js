6b22b32041fe0dbce4477ade762f1df97783a2eb
Ruhani Makhija <ruhani.makhija@brightchamps.com>
2024-06-09 20:54:48 +0530

    added tailwind css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import "./App.css"
 
 const App = () => {
   return (
     <div>
-      Hello
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </div>
   )
 }
